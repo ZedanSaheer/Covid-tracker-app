b440f70f6d46909edc617df8d67e816c6e0886e6
Zedan <zedansaheer.dev@gmail.com>
2021-07-28 01:01:30 +0530

    first ever react app

--- src/components/covid.js
+++ src/components/covid.js
@@ -23,28 +23,28 @@ const Covid = () => {
     return (
         <div>
                 <div className="container">
-                    <div className="covid-card">
+                    <div className="covid-card country">
                         <div className="text-one"><p><span className="one">total</span><span className="two">Cases</span></p></div>
                         <div className="text-two"><p>India</p></div>
                     </div>
-                    <div className="covid-card">
+                    <div className="covid-card recovered">
                         <div className="text-one"><p><span className="one">total</span><span className="two">recovered</span></p></div>
                         <div className="text-two"><p>{data.recovered}</p></div>
                     </div>
-                    <div className="covid-card">
+                    <div className="covid-card confirmed">
                         <div className="text-one"><p><span className="one">total</span><span className="two">confirmed</span></p></div>
                         <div className="text-two"><p>{data.confirmed}</p></div>
                     </div>
-                    <div className="covid-card">
+                    <div className="covid-card death">
                         <div className="text-one"><p><span className="one">total</span><span className="two">death</span></p></div>
                         <div className="text-two"><p>{data.deaths}</p></div>
                     </div>
-                    <div className="covid-card">
+                    <div className="covid-card active">
                         <div className="text-one"><p><span className="one">total</span><span className="two">active</span></p></div>
                         <div className="text-two"><p>{data.active}</p></div>
                     </div>
-                    <div className="covid-card">
-                        <div className="text-one"><p><span className="one">last</span><span className="two">updates</span></p></div>
+                    <div className="covid-card updates">
+                        <div className="text-one"><p><span className="one">last</span><span className="two">updated</span></p></div>
                         <div className="text-two"><p>{data.lastupdatedtime}</p></div>
                     </div>
                 </div>
